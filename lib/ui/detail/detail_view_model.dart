28324c5751039de887413d79d67ca001d2e69a2c
VepTea <97ghddlstjs@gmail.com>
2025-08-04 17:20:09 +0900

    홈, 디테일 페이지 ui 초기설정 끝, main함수에 home에 HomePage연결, 리버팟패키지 pub add함.

--- lib/ui/detail/detail_view_model.dart
+++ lib/ui/detail/detail_view_model.dart
@@ -1,13 +1,1 @@
-import 'package:flutter/material.dart';
 
-class DetailPage extends StatelessWidget {
-  const DetailPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Detail Page')),
-      body: Text('Detail Page'),
-    );
-  }
-}
